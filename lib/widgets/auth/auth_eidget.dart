da23fa4b76400cba1407a32e4571a7878927a1a7
Rozum Sergey <rozumsergei@gmail.com>
2022-04-15 18:50:59 +0800

    addBar

--- lib/widgets/auth/auth_eidget.dart
+++ lib/widgets/auth/auth_eidget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AuthWidget extends StatefulWidget {
-  const AuthWidget({ Key? key }) : super(key: key);
+  const AuthWidget({Key? key}) : super(key: key);
 
   @override
   _Auth_WidgetState createState() => _Auth_WidgetState();
@@ -11,8 +11,19 @@ class _Auth_WidgetState extends State<AuthWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      
+      appBar: AppBar(
+        title: const Text("2 Comit"),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('This is SnackBar'))
+              );
+            },
+            icon: const Icon(Icons.navigate_next),
+          )
+        ],
+      ),
     );
   }
 }

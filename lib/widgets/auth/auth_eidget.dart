8193bf5894e092e9b88e1f00ed95b815c197e130
Rozum Sergey <rozumsergei@gmail.com>
2022-04-15 15:04:45 +0800

    first comit

---
+++ lib/widgets/auth/auth_eidget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AuthWidget extends StatefulWidget {
+  const AuthWidget({ Key? key }) : super(key: key);
+
+  @override
+  _Auth_WidgetState createState() => _Auth_WidgetState();
+}
+
+class _Auth_WidgetState extends State<AuthWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      
+    );
+  }
+}
